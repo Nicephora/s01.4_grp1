94260823eb446d977dd716943994353fd510d7e8
Kawsay <cc.lle@protonmail.com>
2019-04-04 18:15:14 +0200

    fix indentation

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -30,32 +30,32 @@ def full_pyramid
 end
 
 
-    def wtf_pyramid
-        floors = 0
-        bricks = "#"
+def wtf_pyramid
+    floors = 0
+    bricks = "#"
 
-        puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ? (impair)"
+    puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ? (impair)"
+    print "> "
+    user_input = gets.chomp.to_i
+    void = " " * (user_input)
+
+    while (user_input % 2 == 0)
+        puts "NOP. Essaye encore (IM-PAIR)"
         print "> "
         user_input = gets.chomp.to_i
-        void = " " * (user_input)
-
-        while (user_input % 2 == 0)
-            puts "NOP. Essaye encore (IM-PAIR)"
-            print "> "
-            user_input = gets.chomp.to_i
-        end
-        while floors < user_input do floors += 1
-            if floors < (user_input / 2) + 1
-                puts void + bricks
-                void = void.chop
-                bricks += "##"
-            else
-                puts void + bricks
-                void += " "
-                bricks = bricks.chop.chop
-            end
+    end
+    while floors < user_input do floors += 1
+        if floors < (user_input / 2) + 1
+            puts void + bricks
+            void = void.chop
+            bricks += "##"
+        else
+            puts void + bricks
+            void += " "
+            bricks = bricks.chop.chop
         end
     end
+end
 
 half_pyramid    
 full_pyramid
